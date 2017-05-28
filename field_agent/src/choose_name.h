a2a28ba8a838a4b3d5d1d2468a1c09d4b83f0581
IsaacGluck <isaacbgluck@gmail.com>
2017-05-28 11:44:22 -0400

    modularizing

---
+++ field_agent/src/choose_name.h
@@ -0,0 +1,17 @@
+/*****************************************************************/
+/* This program          */
+/*****************************************************************/
+#ifndef __CHOOSE_NAME_H
+#define __CHOOSE_NAME_H
+
+#include <pebble.h>
+
+
+// Choose Name definitions
+#define CHOOSE_NAME_WINDOW_NUM_ROWS     4
+#define CHOOSE_NAME_WINDOW_CELL_HEIGHT  44
+#define CHOOSE_NAME_WINDOW_RADIO_RADIUS 6
+
+char* choose_name_window_push();
+
+#endif
