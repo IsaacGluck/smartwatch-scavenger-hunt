18f28232a4575898fa85bd18998c2c4abb08433d
Morgan Sorbaro <ms.20@dartmouth.edu>
2017-05-30 12:31:26 -0400

    trying to pull confused

--- guide_agent/guideagent.c
+++ guide_agent/guideagent.c
@@ -203,6 +203,7 @@ int main(const int argc, char *argv[]){
       		}
 
       	if (FD_ISSET(comm_sock, &rfds)) {
+      		printf("RECIEVING METHODS\n");
 			handle_socket(comm_sock, &them, game);
      	 }
 
